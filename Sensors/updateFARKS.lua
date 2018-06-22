6df3e9b73bd063738889117fc064d0e35637a82c
Petr Houška <houskape@gmail.com>
2018-06-22 11:43:32 +0200

    FIx update&manage FARKS

--- Sensors/updateFARKS.lua
+++ Sensors/updateFARKS.lua
@@ -15,17 +15,13 @@ function getInfo()
 end
 
 function initFarksInfo()
-	farcks = {}
+	local nfo = {}
 
-	farcks.laneAss = {}
-	farcks.uids = {}
-	farcks.inLine = {}
+	nfo.indexToUid = {}
+	nfo.uidToIndex = {}
+	nfo.laneAss = {}
 
-	for i = 1, 3 do
-		farcks.inLine[i] = 0
-	end
-
-	return farcks
+	return nfo
 end
 
 local SpringGetUnitDefID = Spring.GetUnitDefID
@@ -33,34 +29,29 @@ return function()
 	if bb.farcks == nil then bb.farcks = initFarksInfo() end
 
 	local myUnits = bb.unitsInfo.my
+	local nfo = initFarksInfo()
 
-	local farcks = {}
-	local farcksUids = {}
-    local farcksInLine = {}
-	for i = 1, 3 do
-		farcksInLine[i] = 0
-	end
-
+	local index = 1
 	for i = 1, #myUnits do
 
 		local uid = myUnits[i]
 		local defID = SpringGetUnitDefID(uid)
 		if UnitDefs[defID].humanName == "FARCK" then
+			nfo.uidToIndex[uid] = index
+			nfo.indexToUid[index] = uid
+			index = index + 1
+		
 			-- farck is still alive, copy previous state else is new
-			if bb.farcks[uid] ~= nil then 
-				farcks[uid] = bb.farcks[uid] 
-				farcksInLine[bb.farcks[uid]] = farcksInLine[bb.farcks[uid]] + 1
+			if bb.farcks.uidToIndex[uid] ~= nil then 
+				nfo.laneAss[uid] = bb.farcks.laneAss[uid] 
 			else 
-				farcks[uid] = 0 
+				nfo.laneAss[uid] = 0 
 			end
-			farcksUids[#farcksUids + 1] = uid
 		end
 
 	end
 
-	bb.farcks.laneAss = farcks
-	bb.farcks.uids = farcksUids
-	bb.farcks.inLine = farcksInLine
+	bb.farcks = nfo
 
 	return {}
 end
