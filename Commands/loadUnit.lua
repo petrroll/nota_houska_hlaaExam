78c5c0933ead1eab1bbdc205d75688129de3d38f
Petr Houška <houskape@gmail.com>
2018-06-22 09:55:18 +0200

    Reworked loading/unloading units.

---
+++ Commands/loadUnit.lua
@@ -0,0 +1,45 @@
+function getInfo()
+	return {
+		onNoUnits = RUNNING, -- instant success
+		tooltip = "Loads a unit to a transporter.",
+		parameterDefs = {
+			{ 
+				name = "transporter",
+				variableType = "expression",
+				componentType = "editBox",
+				defaultValue = "",
+			},
+			{ 
+				name = "unit", 
+				variableType = "expression",
+				componentType = "editBox",
+				defaultValue = "",
+            },
+		}
+	}
+end
+
+
+-- speed-ups
+local SpringGiveOrderToUnit = Spring.GiveOrderToUnit
+local SpringGetUnitCommands = Spring.GetUnitCommands
+local SpringGetUnitIsTransporting = Spring.GetUnitIsTransporting
+
+function Run(self, units, parameter)
+    local target_unit = parameter.unit
+    local transport = transporter
+
+    if #SpringGetUnitCommands(transport, 1) > 0 then
+        return RUNNING
+    end
+
+    if not SpringGetUnitIsTransporting(transport) then
+        SpringGiveOrderToUnit(transport, CMD.LOAD_UNITS, { target_unit }, CMD.OPT_SHIFT)
+        return RUNNING
+    end
+
+    return SUCCESS
+end
+
+function Reset(self)
+end
---
+++ Commands/unloadUnit.lua
@@ -0,0 +1,52 @@
+function getInfo()
+	return {
+		onNoUnits = SUCCESS, -- instant success
+		tooltip = "Loads all units to transporter.",
+		parameterDefs = {
+			{ 
+				name = "transporter",
+				variableType = "expression",
+				componentType = "editBox",
+				defaultValue = "",
+			},
+			{ 
+				name = "target", 
+				variableType = "expression",
+				componentType = "editBox",
+				defaultValue = "",
+			},
+			{
+                name = "radius",
+                variableType = "expression",
+                componentType = "editBox",
+                defaultValue = "150",
+            }
+		}
+	}
+end
+
+-- speed-ups
+local SpringGiveOrderToUnit = Spring.GiveOrderToUnit
+local SpringGetUnitCommands = Spring.GetUnitCommands
+local SpringGetUnitIsTransporting = Spring.GetUnitIsTransporting
+
+function Run(self, units, parameter)
+    local target = parameter.target
+    local radius = parameter.radius
+    local transport = transporter
+
+    if #SpringGetUnitCommands(transport, 1) > 0 then
+        return RUNNING
+    end
+
+    if SpringGetUnitIsTransporting(transport) ~= nil then
+        SpringGiveOrderToUnit(transport, CMD.UNLOAD_UNITS, { target.x, target.y, target.z, radius }, CMD.OPT_SHIFT)
+        SpringGiveOrderToUnit(transport, CMD.MOVE, target:AsSpringVector(), CMD.OPT_SHIFT)
+        return RUNNING
+    end
+
+    return SUCCESS
+end
+
+function Reset(self)
+end
